c9653a0de61523904bd022d86dca21397e121b06
Joshua <joshua.adille14@gmail.com>
2023-10-19 13:29:50 +0800

    added sharedservice to the app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { LuxonDateAdapter } from '@angular/material-luxon-adapter';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
+import {
+    PreloadAllModules,
+    provideRouter,
+    withInMemoryScrolling,
+    withPreloading,
+} from '@angular/router';
 import { provideFuse } from '@fuse';
 import { appRoutes } from 'app/app.routes';
 import { getAuth, provideAuth } from '@angular/fire/auth';
@@ -15,32 +20,33 @@ import { provideTransloco } from 'app/core/transloco/transloco.provider';
 import { mockApiServices } from 'app/mock-api';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { firebaseConfig } from 'firebase.config';
-
+import { SharedService } from './shared.service';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideAnimations(),
         provideHttpClient(),
-        provideRouter(appRoutes,
+        provideRouter(
+            appRoutes,
             withPreloading(PreloadAllModules),
-            withInMemoryScrolling({scrollPositionRestoration: 'enabled'}),
+            withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
         ),
 
         // Material Date Adapter
         {
-            provide : DateAdapter,
+            provide: DateAdapter,
             useClass: LuxonDateAdapter,
         },
         {
-            provide : MAT_DATE_FORMATS,
+            provide: MAT_DATE_FORMATS,
             useValue: {
-                parse  : {
+                parse: {
                     dateInput: 'D',
                 },
                 display: {
-                    dateInput         : 'DDD',
-                    monthYearLabel    : 'LLL yyyy',
-                    dateA11yLabel     : 'DD',
+                    dateInput: 'DDD',
+                    monthYearLabel: 'LLL yyyy',
+                    dateA11yLabel: 'DD',
                     monthYearA11yLabel: 'LLLL yyyy',
                 },
             },
@@ -54,55 +60,54 @@ export const appConfig: ApplicationConfig = {
         provideIcons(),
         provideFuse({
             mockApi: {
-                delay   : 0,
+                delay: 0,
                 services: mockApiServices,
             },
-            fuse   : {
-                layout : 'centered',
-                scheme : 'light',
+            fuse: {
+                layout: 'centered',
+                scheme: 'light',
                 screens: {
                     sm: '600px',
                     md: '960px',
                     lg: '1280px',
                     xl: '1440px',
                 },
-                theme  : 'theme-default',
-                themes : [
+                theme: 'theme-default',
+                themes: [
                     {
-                        id  : 'theme-default',
+                        id: 'theme-default',
                         name: 'Default',
                     },
                     {
-                        id  : 'theme-brand',
+                        id: 'theme-brand',
                         name: 'Brand',
                     },
                     {
-                        id  : 'theme-teal',
+                        id: 'theme-teal',
                         name: 'Teal',
                     },
                     {
-                        id  : 'theme-rose',
+                        id: 'theme-rose',
                         name: 'Rose',
                     },
                     {
-                        id  : 'theme-purple',
+                        id: 'theme-purple',
                         name: 'Purple',
                     },
                     {
-                        id  : 'theme-amber',
+                        id: 'theme-amber',
                         name: 'Amber',
                     },
                 ],
             },
         }),
-        importProvidersFrom(
-            [
-                provideFirebaseApp(() => initializeApp(firebaseConfig)),
-                provideAuth(() => getAuth()),
-                provideFirestore(() => getFirestore()),
-                provideFunctions(() => getFunctions()),
-                provideStorage(() => getStorage()),
-            ]
-        )
+        importProvidersFrom([
+            provideFirebaseApp(() => initializeApp(firebaseConfig)),
+            provideAuth(() => getAuth()),
+            provideFirestore(() => getFirestore()),
+            provideFunctions(() => getFunctions()),
+            provideStorage(() => getStorage()),
+        ]),
+        SharedService,
     ],
 };
